eab00553f247892ae5b5048456422754db90a235
Jae-Yun Kim <jaeykim@altair.snu.ac.kr>
2019-04-02 12:34:09 +0900

    transaction template

---
+++ transaction.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"types"
+)
+
+type Transaction struct {
+	data txdata
+	hash types.Hash 
+}
+
+
+// txdata could be generated between more than 2 participants
+// For example, if A, B, C are participants, data of txdata is 
+// participants: [A, B, C]
+// participantNonces: [10, 3, 5]
+// XORs : ['1234', '3245', '4313']
+// Payload : ""
+
+type txdata struct {
+	Participants		[]*types.Address
+	ParticipantNonces	[]uint64
+	XORs				[]uint64
+	Payload				[]byte
+	
+	// Signature values
+}
---
+++ types.go
@@ -0,0 +1,15 @@
+// Block components are based on go-ethereum/common/types.go
+
+package types 
+
+import "fmt"
+
+const (
+	HashLength		= 32
+	AddressLength	= 20
+)
+
+// Hash represents the 32 byte Keccak256 hash of arbitrary data.
+type Hash [HashLength]byte
+
+type Address [AddressLength]byte
