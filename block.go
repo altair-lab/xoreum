99378480449709bb2b198ed27bdd48a5036d0ded
ricegood <yeonjaekoo@gmail.com>
2019-04-02 14:57:32 +0900

    add state field

--- block.go
+++ block.go
@@ -6,13 +6,14 @@ import (
 )
 
 type Header struct {
-	ParentHash	types.Hash
-	Coinbase	types.Address
-	Root		types.Hash
-	TxHash		types.Hash
-	Difficulty	uint64
-	Time		uint64
-	Nonce		uint64
+	ParentHash	types.Hash	`parentHash`
+	Coinbase	types.Address	`miner`
+	Root		types.Hash	`stateRoot`
+	TxHash		types.Hash	`transactionsRoot`
+	State		State		`state`
+	Difficulty	uint64		`difficulty`
+	Time		uint64		`timestamp`
+	Nonce		uint64		`nonce`
 }
 
 type Block struct {
