a91e2004a71e3225e72a92a9a842a7385ab9224f
jmlee <jmlee@altair.snu.ac.kr>
2019-04-02 17:33:27 +0900

    now can run test.go

--- test.go
+++ test.go
@@ -13,21 +13,44 @@ func main() {
 	fmt.Println("---test common/types.go---")
 	hl := common.HashLength
 	al := common.AddressLength
-	//hash1 := common.Hash
-	//address1 := common.Address
+	hash1 := common.Hash{}
+	address1 := common.Address{}
+
+	fmt.Println(hl)
+	fmt.Println(al)
+	fmt.Println(hash1)
+	fmt.Println(address1)
+
+
 
 	fmt.Println("---test core---")
 	blockchain1 := core.BlockChain{}
+	
+	fmt.Println(blockchain1)
+
+
 
 	fmt.Println("---test core/state---")
-	state1 := state.State
+	state1 := state.State{}
 	account1 := state.Account{}
+	
+	fmt.Println(state1)
+	fmt.Println(account1)
+
+
 
 	fmt.Println("---test core/types---")
 	header1 := types.Header{}
 	block1 := types.Block{}
 	transaction1 := types.Transaction{}
+	fmt.Println(header1)
+	fmt.Println(block1)
+	fmt.Println(transaction1)
 
 	fmt.Println("---all test passed---")
+
+
+
+
 
 }
