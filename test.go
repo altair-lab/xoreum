e323f56ee00c7a5677e83096c38ecb4feeff3f20
ricegood <yeonjaekoo@gmail.com>
2019-04-05 17:37:29 +0900

    Update test.go

--- test.go
+++ test.go
@@ -33,44 +33,6 @@ func main() {
 	// Mining
 	fmt.Println("===== Start Mining (Miner : account2) =====")
 	miner := miner.Miner{acc2.Address}
-	block := miner.Mine(*tx, state)
-	fmt.Println(block)
-
-
-
-	/*
-	fmt.Println("---test common/types.go---")
-	hl := common.HashLength
-	al := common.AddressLength
-	hash1 := common.Hash{}
-	address1 := common.Address{}
-	fmt.Println(hl)
-	fmt.Println(al)
-	fmt.Println(hash1)
-	fmt.Println(address1)
-
-	fmt.Println("---test core---")
-	blockchain1 := core.BlockChain{}
-	fmt.Println(blockchain1)
-
-	fmt.Println("---test core/state---")
-	state1 := state.State{}
-	account1 := state.Account{}
-	fmt.Println(state1)
-	fmt.Println(account1)
-
-	fmt.Println("---test core/types---")
-	header1 := types.Header{}
-	block1 := types.Block{}
-	transaction1 := types.Transaction{}
-	fmt.Println(header1)
-	fmt.Println(block1)
-	fmt.Println(transaction1)
-
-	fmt.Println("---test core/crypto---")
-	keccak256_1 := crypto.Keccak256()
-	fmt.Println(keccak256_1)
-
-	fmt.Println("---all test passed---")
-*/
+	block := miner.Mine(*tx, state, 240) // Difficulty is 0~255 for now
+	fmt.Println("Created Block: " , block)
 }
