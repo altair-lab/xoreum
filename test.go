98a38e466b9a81dfaffa30c5295a7fcae891f52c
ricegood <yeonjaekoo@gmail.com>
2019-04-05 16:58:17 +0900

    Update test.go

--- test.go
+++ test.go
@@ -3,14 +3,42 @@ package main
 import (
 	"fmt"
 	"github.com/altair-lab/xoreum/common"
-	"github.com/altair-lab/xoreum/core"
+	//"github.com/altair-lab/xoreum/core"
 	"github.com/altair-lab/xoreum/core/state"
 	"github.com/altair-lab/xoreum/core/types"
-	"github.com/altair-lab/xoreum/crypto"
+	"github.com/altair-lab/xoreum/core/miner"
+	//"github.com/altair-lab/xoreum/crypto"
 )
 
 func main() {
+	// send "10" to "account 1"
+	
+	// Create default accounts
+	fmt.Println("===== Create default accounts . . . =====")
+	acc0 := state.NewAccount(common.Address{0}, uint64(0), uint64(100)) // acc0 [Address:0, Nonce:0, Balance:100]
+	acc1 := state.NewAccount(common.Address{1}, uint64(0), uint64(100)) // acc1 [Address:1, Nonce:0, Balance:100]
+	acc2 := state.NewAccount(common.Address{2}, uint64(0), uint64(0)) // acc1 [Address:1, Nonce:0, Balance:0]
+	fmt.Println("Account0: ", acc0)
+	fmt.Println("Account1: ", acc1)
+	fmt.Println("Account2: ", acc2)
 
+	// [TODO]Set state
+	state := state.State{}
+
+	// Create transaction (send "10" from "account0" to "account1")
+	fmt.Println("===== Create transaction that send [10] from [account0] to [account1] =====")
+	tx := types.NewTransaction(acc0.Address, acc1.Address, uint64(10))
+	fmt.Println("Transaction: ", tx)
+
+	// Mining
+	fmt.Println("===== Start Mining (Miner : account2) =====")
+	miner := miner.Miner{acc2.Address}
+	block := miner.Mine(*tx, state)
+	fmt.Println(block)
+
+
+
+	/*
 	fmt.Println("---test common/types.go---")
 	hl := common.HashLength
 	al := common.AddressLength
@@ -44,5 +72,5 @@ func main() {
 	fmt.Println(keccak256_1)
 
 	fmt.Println("---all test passed---")
-
+*/
 }
