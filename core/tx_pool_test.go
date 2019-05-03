1cf54ea7dd0fbd54b02f63ccebc058195060ef5d
ricegood <yeonjaekoo@gmail.com>
2019-05-03 15:46:51 +0900

    Update tx_pool_test

--- core/tx_pool_test.go
+++ core/tx_pool_test.go
@@ -1,6 +1,4 @@
-package test
-
-/*package test
+package core 
 
 import (
 	"fmt"
@@ -10,36 +8,39 @@ import (
 	"github.com/altair-lab/xoreum/core/miner"
 	"github.com/altair-lab/xoreum/core/state"
 	"github.com/altair-lab/xoreum/core/types"
-	"github.com/altair-lab/xoreum/core/miner"
+	"github.com/altair-lab/xoreum/crypto"
 	
 	//"github.com/davecgh/go-spew/spew"
 )
 
 
 func ExampleTxpool() {
-
-	// Make keys
+	
+	// Make accounts
 	fmt.Println("========== Create Accounts ==========")
 	privatekey0, _ := crypto.GenerateKey()
 	publickey0 := privatekey0.PublicKey
 	address0 := crypto.Keccak256Address(common.ToBytes(publickey0))
-
+	acc0 := state.NewAccount(address0, uint64(0), uint64(7000)) // acc0 [Address:0, Nonce:0, Balance:7000]
+	
+	state := state.NewState()
+	state.Add(acc0)
+	state.Print()
+	
+	/*
 	privatekey1, _ := crypto.GenerateKey()
 	publickey1 := privatekey1.PublicKey
 	address1 := crypto.Keccak256Address(common.ToBytes(publickey1))
+	acc1 := state.NewAccount(address1, uint64(0), uint64(2000)) // acc1 [Address:1, Nonce:0, Balance:2000]
 
 	privatekey2, _ := crypto.GenerateKey()
 	publickey2 := privatekey2.PublicKey
 	address2 := crypto.Keccak256Address(common.ToBytes(publickey2))
+	acc2 := state.NewAccount(address2, uint64(0), uint64(100))  // acc2 [Address:2, Nonce:0, Balance:100]
 
 	privatekey3, _ := crypto.GenerateKey()
 	publickey3 := privatekey3.PublicKey
 	address3 := crypto.Keccak256Address(common.ToBytes(publickey3))
-
-	// Make account
-	acc0 := state.NewAccount(address0, uint64(0), uint64(7000)) // acc0 [Address:0, Nonce:0, Balance:7000]
-	acc1 := state.NewAccount(address1, uint64(0), uint64(2000)) // acc1 [Address:1, Nonce:0, Balance:2000]
-	acc2 := state.NewAccount(address2, uint64(0), uint64(100))  // acc2 [Address:2, Nonce:0, Balance:100]
 	acc3 := state.NewAccount(address3, uint64(0), uint64(100))  // acc3 [Address:3, Nonce:0, Balance:100]
 
 	// [TODO] Set state
@@ -52,7 +53,7 @@ func ExampleTxpool() {
 
 	fmt.Printf("\n")
 
-	// Create tranaction
+	// Create transaction
 	fmt.Println("========== Create Transactions ==========")
 	tx0 := types.NewTransaction(0, publickey0, publickey1, uint64(2000)) // send [2000] from [account0] to [account1]
 	tx1 := types.NewTransaction(0, publickey1, publickey0, uint64(500)) // send [500] from [account1] to [account0]
@@ -76,6 +77,13 @@ func ExampleTxpool() {
 	tx2_signed, _ = types.SignTx(tx2_signed, privatekey1) // sign by receiver
 	tx_insufficient_signed, _ = types.SignTx(tx_insufficient_signed, privatekey2) // sign by receiver
 	tx_sender_invalid_signed, _ = types.SignTx(tx_sender_invalid_signed, privatekey1) // sign by receiver
+	*/
+
+	fmt.Println("========== Create Txs ==========")
+	// Make Test Signed Tx
+	tx0_signed := types.MakeTestSignedTx(2)
+	tx1_signed := types.MakeTestSignedTx(3)
+	tx_sender_invalid_signed := types.MakeTestTx(2)
 
 	// Create Chain, txpool
 	bc := core.NewBlockChain()
@@ -91,7 +99,7 @@ func ExampleTxpool() {
 	if !success {
 		fmt.Println(err)
 	}
-
+	/*
 	success, err = txpool.Add(tx_overwrite_signed)
 	if !success {
 		fmt.Println(err)
@@ -106,17 +114,14 @@ func ExampleTxpool() {
 	if !success {
 		fmt.Println(err)
 	}
+	*/
 	
-	/*
 	// [TODO] return false (not runtime error!) when the sign is invalid
 	success, err = txpool.Add(tx_sender_invalid_signed)
 	if !success {
 		fmt.Println(err)
 	}
-	*/
-
-	fmt.Printf("\n")
-
+	
 	// Mining from txpool
 	fmt.Println("============ Mining block  ============")
 	miner := miner.Miner{acc0.Address}
@@ -139,4 +144,3 @@ func ExampleTxpool() {
 	// output:
 	// true
 }
-*/
