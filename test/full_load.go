a4d3461338de7aa4740cf689fc853fdc3199d501
eric-yoo <ajoije729@gmail.com>
2019-05-06 18:39:29 +0900

    rename to loadblockbybn, TODO func in comment

--- test/full_load.go
+++ test/full_load.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"fmt"
 
 	//"bufio"
 	//"strconv"
+
+	"fmt"
 
 	"github.com/altair-lab/xoreum/core/rawdb"
 	"github.com/altair-lab/xoreum/xordb/leveldb"
@@ -19,11 +20,15 @@ func main() {
 	last_hash := rawdb.ReadLastHeaderHash(db)
 	last_BN := rawdb.ReadHeaderNumber(db, last_hash)
 
-	for i := uint64(0); i < uint64(*last_BN); i++ {
+	for i := uint64(1); i <= uint64(*last_BN); i++ {
 		//load block via accessor api
-		fmt.Println("loading block", i+1)
-		rawdb.LoadBlockN(db, i+1)
+		fmt.Println("loading block", i)
+		rawdb.LoadBlockByBN(db, i)
 		fmt.Println("\n")
 	}
 
+	// ldb, _ := leveldb.New("chaindata", 0, 0, "")
+
+	// blockchain := rawdb.LoadBlockChain(ldb)
+	// blockchain.PrintBlockChain()
 }
