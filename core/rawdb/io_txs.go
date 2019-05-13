5c17daec3f9a4ecd030dd752ed0ac849721b2682
eric-yoo <ajoije729@gmail.com>
2019-05-13 19:30:59 +0900

    db i/o for transaction

--- core/rawdb/io_txs.go
+++ core/rawdb/io_txs.go
@@ -1,6 +1,7 @@
 package rawdb
 
 import (
+	"fmt"
 	"math/big"
 
 	"github.com/altair-lab/xoreum/common"
@@ -9,33 +10,25 @@ import (
 	"github.com/altair-lab/xoreum/xordb"
 )
 
-// ReadTxLookupEntry retrieves the positional metadata associated with a transaction
-// hash to allow retrieving the transaction or receipt by hash.
+// ReadTxLookupEntry retrieves the blocknumber of the block the tx is in
 func ReadTxLookupEntry(db xordb.Reader, hash common.Hash) *uint64 {
 	data, _ := db.Get(txLookupKey(hash))
 	if len(data) == 0 {
 		return nil
 	}
-	// Database v6 tx lookup just stores the block number
 	if len(data) < common.HashLength {
 		number := new(big.Int).SetBytes(data).Uint64()
 		return &number
 	}
-	// Database v4-v5 tx lookup format just stores the hash
-	if len(data) == common.HashLength {
-		return ReadHeaderNumber(db, common.BytesToHash(data))
-	}
-
 	return nil
 }
 
-// WriteTxLookupEntries stores a positional metadata for every transaction from
-// a block, enabling hash based transaction lookups.
+// WriteTxLookupEntries stores the blocknumber of the block the tx is in
 func WriteTxLookupEntries(db xordb.Writer, block *types.Block) {
 
 	num := common.Num2Bytes(block.Number())
 	for _, tx := range block.Transactions() {
-		if err := db.Put(txLookupKey(tx.Hash()), num); err != nil {
+		if err := db.Put(txLookupKey(tx.Hash), num); err != nil {
 			log.Crit("Failed to store transaction lookup entry", "err", err)
 		}
 	}
@@ -46,24 +39,28 @@ func DeleteTxLookupEntry(db xordb.Writer, hash common.Hash) {
 	db.Delete(txLookupKey(hash))
 }
 
-// ReadTransaction retrieves a specific transaction from the database, along with
-// its added positional metadata. (tx, blockHash, *blockNumber, uint64(txIndex))
+// ReadTransaction retrieves a transaction and its metadata
+// (tx, blockHash, *blockNumber, uint64(txIndex))
 func ReadTransaction(db xordb.Reader, hash common.Hash) (*types.Transaction, common.Hash, uint64, uint64) {
+
 	blockNumber := ReadTxLookupEntry(db, hash)
 	if blockNumber == nil {
+		fmt.Println("case1")
 		return nil, common.Hash{}, 0, 0
 	}
 	blockHash := ReadHash(db, *blockNumber)
 	if blockHash == (common.Hash{}) {
+		fmt.Println("case2")
 		return nil, common.Hash{}, 0, 0
 	}
 	block := LoadBlock(db, blockHash, *blockNumber)
 	if block == nil {
+		fmt.Println("case3")
 		log.Error("Transaction referenced missing", "number", blockNumber, "hash", blockHash)
 		return nil, common.Hash{}, 0, 0
 	}
 	for txIndex, tx := range block.Transactions() {
-		if tx.Hash() == hash {
+		if tx.Hash == hash {
 			return tx, blockHash, *blockNumber, uint64(txIndex)
 		}
 	}
