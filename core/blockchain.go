a0ec9e6f8ac183d68bee911d9f20e3d0121ec9ff
jmlee <jmlee@altair.snu.ac.kr>
2019-05-14 12:33:36 +0900

    Now MakeTestBlockChain() insert meaningful txs

--- core/blockchain.go
+++ core/blockchain.go
@@ -14,6 +14,7 @@ import (
 	"github.com/altair-lab/xoreum/xordb/memorydb"
 
 	"github.com/altair-lab/xoreum/common"
+	"github.com/altair-lab/xoreum/core/state"
 	"github.com/altair-lab/xoreum/core/types"
 	"github.com/altair-lab/xoreum/params"
 )
@@ -33,6 +34,8 @@ type BlockChain struct {
 	//ChainID *big.Int // chainId identifies the current chain and is used for replay protection
 
 	db xordb.Database
+
+	s state.State // temporary field before import db APIs
 
 	genesisBlock *types.Block
 	currentBlock atomic.Value
@@ -127,27 +130,129 @@ func (bc *BlockChain) PrintBlockChain() {
 }
 
 // make blockchain for test. insert simple blocks
-func MakeTestBlockChain(chainLength uint64) *BlockChain {
+func MakeTestBlockChain(chainLength uint64, partNum uint64) *BlockChain {
 
 	db := memorydb.New()
 	bc := NewBlockChain(db)
+	allTxs := make(map[common.Hash]*types.Transaction) // all txs in this test blockchain
+	userCurTx := make(map[int64]*common.Hash)          // map to fill PrevTxHashes of tx
 
-	pubkeys := []*ecdsa.PublicKey{}
-	for i := 0; i < 1000; i++ {
+	// initialize random users
+	privkeys := []*ecdsa.PrivateKey{}
+	accounts := []*state.Account{}
+	for i := uint64(0); i < partNum; i++ {
 		priv, _ := crypto.GenerateKey()
-		pubkeys = append(pubkeys, &priv.PublicKey)
+		privkeys = append(privkeys, priv)
+		acc := state.NewAccount(&priv.PublicKey, 0, 100)
+		accounts = append(accounts, acc)
+		userCurTx[int64(i)] = &common.Hash{} // initialize: nil Tx
 	}
 
-	// insert blocks into blockchain
+	// make and insert blocks into blockchain
 	for i := uint64(1); i <= chainLength; i++ {
 
-		randNum, _ := rand.Int(rand.Reader, big.NewInt(100))
-		fmt.Println(randNum)
+		// make random transactions
 
+		// make empty Transactions
 		txs := make(types.Transactions, 0)
-		txs.Insert(types.MakeTestSignedTx(2))
-		txs.Insert(types.MakeTestSignedTx(3))
 
+		// make and insert random tx into txs
+		txnum := 2 // max tx num per block
+		for i := 0; i < txnum; i++ {
+			randNumber, _ := rand.Int(rand.Reader, big.NewInt(3)) // 0 ~ 2
+			randNum := randNumber.Int64()                         // convert big.int to int
+			if randNum == 0 {
+				// do not insert tx
+				continue
+			}
+
+			// fields for random tx
+			parPublicKeys := []*ecdsa.PublicKey{}
+			parStates := []*state.Account{}
+			prevTxHashes := []*common.Hash{}
+
+			// make random tx and add it into txs
+			if randNum == 1 {
+				// tx's participants number: 2
+
+				// pick 2 random numbers
+				R1, _ := rand.Int(rand.Reader, big.NewInt(int64(partNum/2)))
+				R2, _ := rand.Int(rand.Reader, big.NewInt(int64(partNum/2)))
+				r1 := R1.Int64()
+				r2 := R2.Int64() + int64(partNum/2)
+
+				// fill fields for tx
+				parPublicKeys = append(parPublicKeys, accounts[r1].PublicKey)
+				parPublicKeys = append(parPublicKeys, accounts[r2].PublicKey)
+				parStates = append(parStates, accounts[r1])
+				parStates = append(parStates, accounts[r2])
+				prevTxHashes = append(prevTxHashes, userCurTx[r1])
+				prevTxHashes = append(prevTxHashes, userCurTx[r2])
+
+				// make tx
+				tx := types.NewTransaction(parPublicKeys, parStates, prevTxHashes)
+
+				// sign tx to make valid tx
+				tx.Sign(privkeys[r1])
+				tx.Sign(privkeys[r2])
+
+				// update userCurTx
+				h := tx.Hash()
+				userCurTx[r1] = &h
+				userCurTx[r2] = &h
+
+				// insert random tx into txs
+				txs.Insert(tx)
+
+				// save all tx in allTxs
+				allTxs[tx.Hash()] = tx
+
+			} else {
+				// tx's participants number: 3
+
+				// pick 3 random numbers
+				R1, _ := rand.Int(rand.Reader, big.NewInt(int64(partNum/3)))
+				R2, _ := rand.Int(rand.Reader, big.NewInt(int64(partNum/3)))
+				R3, _ := rand.Int(rand.Reader, big.NewInt(int64(partNum/3)))
+				r1 := R1.Int64()
+				r2 := R2.Int64() + int64(partNum/3)
+				r3 := R3.Int64() + int64(partNum/3) + int64(partNum/3)
+
+				// fill fields for tx
+				parPublicKeys = append(parPublicKeys, accounts[r1].PublicKey)
+				parPublicKeys = append(parPublicKeys, accounts[r2].PublicKey)
+				parPublicKeys = append(parPublicKeys, accounts[r3].PublicKey)
+				parStates = append(parStates, accounts[r1])
+				parStates = append(parStates, accounts[r2])
+				parStates = append(parStates, accounts[r3])
+				prevTxHashes = append(prevTxHashes, userCurTx[r1])
+				prevTxHashes = append(prevTxHashes, userCurTx[r2])
+				prevTxHashes = append(prevTxHashes, userCurTx[r3])
+
+				// make tx
+				tx := types.NewTransaction(parPublicKeys, parStates, prevTxHashes)
+
+				// sign tx to make valid tx
+				tx.Sign(privkeys[r1])
+				tx.Sign(privkeys[r2])
+				tx.Sign(privkeys[r3])
+
+				// update userCurTx
+				h := tx.Hash()
+				userCurTx[r1] = &h
+				userCurTx[r2] = &h
+				userCurTx[r3] = &h
+
+				// insert random tx into txs
+				txs.Insert(tx)
+
+				// save all tx in allTxs
+				allTxs[tx.Hash()] = tx
+			}
+
+		}
+
+		// make random block
 		b := types.NewBlock(&types.Header{}, txs)
 		b.GetHeader().ParentHash = bc.CurrentBlock().Hash()
 		b.GetHeader().Number = i
