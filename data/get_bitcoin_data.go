f02c94f7c6d924173afff274d97dd5488b5d351f
jmlee <jmlee@altair.snu.ac.kr>
2019-05-22 03:07:28 +0900

    rpc connection with bitcoin database

--- data/get_bitcoin_data.go
+++ data/get_bitcoin_data.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"crypto/ecdsa"
+	"crypto/tls"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -17,6 +20,7 @@ import (
 	"github.com/altair-lab/xoreum/core/types"
 	"github.com/altair-lab/xoreum/crypto"
 	"github.com/altair-lab/xoreum/xordb/memorydb"
+	"github.com/toorop/go-bitcoind"
 )
 
 type BitcoinBlock struct {
@@ -141,7 +145,7 @@ func GetBitcoinTx(txHash string) *BitcoinTx {
 }
 
 // transform bitcoin data to xoreum's data
-func TransformBitcoinData(targetBlockNum int) *core.BlockChain {
+func TransformBitcoinData(targetBlockNum int, rpc *bitcoind.Bitcoind) *core.BlockChain {
 	db := memorydb.New()
 	bc, genesisPrivateKey := core.NewBlockChainForBitcoin(db) // already has bitcoin's genesis block
 
@@ -163,11 +167,14 @@ func TransformBitcoinData(targetBlockNum int) *core.BlockChain {
 	blockHashes := make(map[int]string)
 
 	// fill blockHashes (TODO: get block hashes automatically later)
-	blockHashes[1] = "00000000839a8e6886ab5951d76f411475428afc90947ee320161bbf18eb6048"
-	blockHashes[2] = "000000006a625f06636b8bb6ac7b960a8d03705d1ace08b1a19da3fdcc99ddbd"
-	blockHashes[3] = "0000000082b5015589a3fdf2d4baff403e6f0be035a5d9742c1cae6295464449"
-	blockHashes[4] = "000000004ebadb55ee9096c9a2f8880e09da59c0d68b1c228da88e48844a1485"
-	blockHashes[5] = "000000009b7262315dbf071787ad3656097b892abffd1f95a1a022f896f533fc"
+	//blockHashes[1] = "00000000839a8e6886ab5951d76f411475428afc90947ee320161bbf18eb6048"
+	//blockHashes[2] = "000000006a625f06636b8bb6ac7b960a8d03705d1ace08b1a19da3fdcc99ddbd"
+	//blockHashes[3] = "0000000082b5015589a3fdf2d4baff403e6f0be035a5d9742c1cae6295464449"
+	//blockHashes[4] = "000000004ebadb55ee9096c9a2f8880e09da59c0d68b1c228da88e48844a1485"
+	//blockHashes[5] = "000000009b7262315dbf071787ad3656097b892abffd1f95a1a022f896f533fc"
+	for i := 1; i <= targetBlockNum; i++ {
+		blockHashes[i], _ = rpc.GetBlockHash(uint64(i))
+	}
 
 	// get blocks of bitcoin and transform into xoreum format
 	for i := 1; i <= targetBlockNum; i++ {
@@ -373,7 +380,12 @@ func (bb *BitcoinBlock) GetValueSum() {
 
 func main() {
 
-	bc := TransformBitcoinData(3)
+	rpc, err := bitcoind.New(SERVER_HOST, SERVER_PORT, USER, PASSWD, USESSL)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	bc := TransformBitcoinData(3, rpc)
 	bc.PrintBlockChain()
 	fmt.Println()
 	bc.GetAccounts().Print()
@@ -390,5 +402,181 @@ func main() {
 	//tx := GetBitcoinTx("6ad0d210305ef6426bd6ac94d618230f48a3e264199608a86bd450b316013f3b")
 	//tx.PrintTx()
 
+	hash, err := rpc.GetBlockHash(500000)
+	log.Println(err, hash)
+
 	// output: 1
+}
+
+// ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
+const (
+	SERVER_HOST        = "sirius.snu.ac.kr"
+	SERVER_PORT        = 8332
+	USER               = "vmolab"
+	PASSWD             = "ma55lab"
+	USESSL             = false
+	WALLET_PASSPHRASE  = "p1"
+	WALLET_PASSPHRASE2 = "p2"
+)
+
+const (
+	// VERSION represents bicoind package version
+	VERSION = 0.1
+	// DEFAULT_RPCCLIENT_TIMEOUT represent http timeout for rcp client
+	RPCCLIENT_TIMEOUT = 30
+)
+
+// A Bitcoind represents a Bitcoind client
+type Bitcoind struct {
+	client *rpcClient
+}
+
+// New return a new bitcoind
+func New(host string, port int, user, passwd string, useSSL bool, timeoutParam ...int) (*Bitcoind, error) {
+	var timeout int = RPCCLIENT_TIMEOUT
+	// If the timeout is specified in timeoutParam, allow it.
+	if len(timeoutParam) != 0 {
+		timeout = timeoutParam[0]
+	}
+
+	rpcClient, err := newClient(host, port, user, passwd, useSSL, timeout)
+	if err != nil {
+		return nil, err
+	}
+	return &Bitcoind{rpcClient}, nil
+}
+
+// A rpcClient represents a JSON RPC client (over HTTP(s)).
+type rpcClient struct {
+	serverAddr string
+	user       string
+	passwd     string
+	httpClient *http.Client
+	timeout    int
+}
+
+// rpcRequest represent a RCP request
+type rpcRequest struct {
+	Method  string      `json:"method"`
+	Params  interface{} `json:"params"`
+	Id      int64       `json:"id"`
+	JsonRpc string      `json:"jsonrpc"`
+}
+
+// rpcError represents a RCP error
+/*type rpcError struct {
+	Code    int16  `json:"code"`
+	Message string `json:"message"`
+}*/
+
+type rpcResponse struct {
+	Id     int64           `json:"id"`
+	Result json.RawMessage `json:"result"`
+	Err    interface{}     `json:"error"`
+}
+
+func newClient(host string, port int, user, passwd string, useSSL bool, timeout int) (c *rpcClient, err error) {
+	if len(host) == 0 {
+		err = errors.New("Bad call missing argument host")
+		return
+	}
+	var serverAddr string
+	var httpClient *http.Client
+	if useSSL {
+		serverAddr = "https://"
+		t := &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		}
+		httpClient = &http.Client{Transport: t}
+	} else {
+		serverAddr = "http://"
+		httpClient = &http.Client{}
+	}
+	c = &rpcClient{serverAddr: fmt.Sprintf("%s%s:%d", serverAddr, host, port), user: user, passwd: passwd, httpClient: httpClient, timeout: timeout}
+	return
+}
+
+// doTimeoutRequest process a HTTP request with timeout
+func (c *rpcClient) doTimeoutRequest(timer *time.Timer, req *http.Request) (*http.Response, error) {
+	type result struct {
+		resp *http.Response
+		err  error
+	}
+	done := make(chan result, 1)
+	go func() {
+		resp, err := c.httpClient.Do(req)
+		done <- result{resp, err}
+	}()
+	// Wait for the read or the timeout
+	select {
+	case r := <-done:
+		return r.resp, r.err
+	case <-timer.C:
+		return nil, errors.New("Timeout reading data from server")
+	}
+}
+
+// call prepare & exec the request
+func (c *rpcClient) call(method string, params interface{}) (rr rpcResponse, err error) {
+	connectTimer := time.NewTimer(time.Duration(c.timeout) * time.Second)
+	rpcR := rpcRequest{method, params, time.Now().UnixNano(), "1.0"}
+	payloadBuffer := &bytes.Buffer{}
+	jsonEncoder := json.NewEncoder(payloadBuffer)
+	err = jsonEncoder.Encode(rpcR)
+	if err != nil {
+		return
+	}
+	req, err := http.NewRequest("POST", c.serverAddr, payloadBuffer)
+	if err != nil {
+		return
+	}
+	req.Header.Add("Content-Type", "application/json;charset=utf-8")
+	req.Header.Add("Accept", "application/json")
+
+	// Auth ?
+	if len(c.user) > 0 || len(c.passwd) > 0 {
+		req.SetBasicAuth(c.user, c.passwd)
+	}
+
+	resp, err := c.doTimeoutRequest(connectTimer, req)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	data, err := ioutil.ReadAll(resp.Body)
+	//fmt.Println(string(data))
+	if err != nil {
+		return
+	}
+	if resp.StatusCode != 200 {
+		err = errors.New("HTTP error: " + resp.Status)
+		return
+	}
+	err = json.Unmarshal(data, &rr)
+	return
+}
+
+// GetBlockHash returns hash of block in best-block-chain at <index>
+func (b *Bitcoind) GetBlockHash(index uint64) (hash string, err error) {
+	r, err := b.client.call("getblockhash", []uint64{index})
+	if err = handleError(err, &r); err != nil {
+		return
+	}
+	err = json.Unmarshal(r.Result, &hash)
+	return
+}
+
+// handleError handle error returned by client.call
+func handleError(err error, r *rpcResponse) error {
+	if err != nil {
+		return err
+	}
+	if r.Err != nil {
+		rr := r.Err.(map[string]interface{})
+		return errors.New(fmt.Sprintf("(%v) %s", rr["code"].(float64), rr["message"].(string)))
+
+	}
+	return nil
 }
