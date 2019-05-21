6d0b2d418765f956d189cb1670d0b654da12350d
jmlee <jmlee@altair.snu.ac.kr>
2019-05-21 15:55:39 +0900

    add AllTxs Print() function

--- core/blockchain.go
+++ core/blockchain.go
@@ -86,8 +86,19 @@ func NewBlockChainForBitcoin(db xordb.Database) (*BlockChain, *ecdsa.PrivateKey)
 
 	bc.accounts = state.NewAccounts()
 	bc.applyTransaction(bc.accounts, bc.genesisBlock.GetTxs())
-	bc.s = state.State{}
+
 	bc.allTxs = types.AllTxs{}
+	genesisTxs := bc.genesisBlock.GetTxs()
+	genesisTxHash := common.Hash{}
+	for _, tx := range *genesisTxs {
+		genesisTxHash = tx.GetHash()
+		bc.allTxs[genesisTxHash] = tx
+	}
+
+	bc.s = state.State{}
+	for k, _ := range bc.accounts {
+		bc.s[k] = genesisTxHash
+	}
 
 	return bc, genesisPrivateKey
 }
